100a0cafb60473afa4fa64c25aa32766e5899b85
kuanjoy <kuanyshmykyev@gmail.com>
2025-09-28 17:45:47 +0600

    chore: show error handling example

---
+++ scripts/11_error_handling.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+)
+
+var (
+	ErrNotImplemented = errors.New("not implemented")
+	ErrTruckNotFound  = errors.New("truck not found")
+)
+
+type Truck struct {
+	id string
+}
+
+// method of the Truck
+func (t *Truck) LoadCargo() error {
+	return nil
+}
+
+func (t *Truck) UnloadCargo() error {
+	return nil
+}
+
+// processTruck handles the loading and unloading of a truck
+func processTruck(truck Truck) error {
+	fmt.Printf("Processing truck: %s\n", truck.id)
+
+	if err := truck.LoadCargo(); err != nil {
+		return fmt.Errorf("Error loading cargo: %w", err)
+	}
+
+	if err := truck.UnloadCargo(); err != nil {
+		return fmt.Errorf("Error loading cargo: %w", err)
+	}
+
+	return nil
+}
+
+func main() {
+
+	trucks := []Truck{
+		{id: "Truck-1"},
+		{id: "Truck-2"},
+		{id: "Truck-3"},
+	}
+
+	for _, truck := range trucks {
+		fmt.Printf("Truck %s arrvied.\n", truck.id)
+
+		if err := processTruck(truck); err != nil {
+			log.Fatalf("Error processing truck: %s", err)
+		}
+	}
+
+}
