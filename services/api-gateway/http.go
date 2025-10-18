5f356fd77f76f346334b879efd35f1b56eb21a2f
kuanori <kuanyshmykyev@gmail.com>
2025-10-18 14:09:36 +0500

    feat: 7-42-43 implement grpc trip handler

--- services/api-gateway/http.go
+++ services/api-gateway/http.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"log"
 	"microservices_with_go/services/api-gateway/grpc_clients"
@@ -23,31 +22,42 @@ func handleTripPreview(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	jsonBody, _ := json.Marshal(reqBody)
-	reader := bytes.NewReader(jsonBody)
-
 	tripService, err := grpc_clients.NewTripServiceClient()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Don't forget to close the client to avoid resource leaks
 	defer tripService.Close()
 
-	resp, err := http.Post("http://trip-service:8083/preview", "application/json", reader)
+	tripPreview, err := tripService.Client.PreviewTrip(r.Context(), reqBody.ToProto())
 	if err != nil {
-		http.Error(w, "failed to call trip service: "+err.Error(), http.StatusInternalServerError)
+		log.Printf("failed to send preview trip: %v", err)
+		http.Error(w, "Failed to preview trip", http.StatusInternalServerError)
 		return
 	}
 
-	defer resp.Body.Close()
+	// ======== HTTP
+	// jsonBody, _ := json.Marshal(reqBody)
+	// reader := bytes.NewReader(jsonBody)
 
-	var respBody any
-	if err := json.NewDecoder(resp.Body).Decode(&respBody); err != nil {
-		http.Error(w, "failed to parse json data from trip service", http.StatusBadRequest)
-		return
-	}
+	// resp, err := http.Post("http://trip-service:8083/preview", "application/json", reader)
+	// if err != nil {
+	// 	http.Error(w, "failed to call trip service: "+err.Error(), http.StatusInternalServerError)
+	// 	return
+	// }
 
-	response := contracts.APIResponse{Data: respBody}
+	// defer resp.Body.Close()
+
+	// var respBody any
+	// if err := json.NewDecoder(resp.Body).Decode(&respBody); err != nil {
+	// 	http.Error(w, "failed to parse json data from trip service", http.StatusBadRequest)
+	// 	return
+	// }
+
+	// response := contracts.APIResponse{Data: respBody}
+
+	response := contracts.APIResponse{Data: tripPreview}
 
 	writeJson(w, http.StatusOK, response)
 }
--- services/trip-service/internal/service/service.go
+++ services/trip-service/internal/service/service.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"microservices_with_go/services/trip-service/internal/domain"
+	tripTypes "microservices_with_go/services/trip-service/pkg/types"
 	"microservices_with_go/shared/types"
 	"net/http"
 
@@ -34,7 +35,7 @@ func (s *service) CreateTrip(ctx context.Context, fare *domain.RideFareModel) (*
 	return s.repo.CreateTrip(ctx, t)
 }
 
-func (s *service) GetRoute(ctx context.Context, pickup, destination *types.Coordinate) (*types.OsrmApiResponse, error) {
+func (s *service) GetRoute(ctx context.Context, pickup, destination *types.Coordinate) (*tripTypes.OsrmApiResponse, error) {
 	url := fmt.Sprintf("http://router.project-osrm.org/route/v1/driving/%f,%f;%f,%f?overview=full&geometries=geojson",
 		pickup.Longitude, pickup.Latitude,
 		destination.Longitude, destination.Latitude,
@@ -51,7 +52,7 @@ func (s *service) GetRoute(ctx context.Context, pickup, destination *types.Coord
 		return nil, fmt.Errorf("failed to read the response: %v", err)
 	}
 
-	var routeResp types.OsrmApiResponse
+	var routeResp tripTypes.OsrmApiResponse
 	if err := json.Unmarshal(body, &routeResp); err != nil {
 		return nil, fmt.Errorf("failed to parse the response: %v", err)
 	}
