5a650fef24c61c7f160481c5d31398fa43c02b59
kuanori <kuanyshmykyev@gmail.com>
2025-11-18 19:35:10 +0500

    feat: 15-92 add bson for domain structs

--- services/trip-service/internal/domain/ride_fare.go
+++ services/trip-service/internal/domain/ride_fare.go
@@ -3,18 +3,16 @@ package domain
 import (
 	tripTypes "microservices_with_go/services/trip-service/pkg/types"
 	pb "microservices_with_go/shared/proto/trip"
-	"time"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type RideFareModel struct {
-	ID                primitive.ObjectID
-	UserID            string // rider id
-	PackageSlug       string // ex: van, luxury, sedan
-	TotalPriceInCents float64
-	Expires           time.Time
-	Route             *tripTypes.OsrmApiResponse
+	ID                primitive.ObjectID         `bson:"_id,omitempty"`
+	UserID            string                     `bson:"userID"`      // rider id
+	PackageSlug       string                     `bson:"packageSlug"` // ex: van, luxury, sedan
+	TotalPriceInCents float64                    `bson:"totalPriceInCents"`
+	Route             *tripTypes.OsrmApiResponse `bson:"route"`
 }
 
 func (r *RideFareModel) ToProto() *pb.RideFare {
